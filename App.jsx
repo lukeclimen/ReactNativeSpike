ebce9c4502727f29833a8823554242a1b5d649e5
Luke Climenhage <luke.climenhage@gmail.com>
2023-12-31 10:54:37 -0500

    Add FlatList component (also Prettier styling)

--- App.jsx
+++ App.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: 1, text: 'Milk'},
+    {id: 2, text: 'Bread'},
+    {id: 3, text: 'Eggs'},
+    {id: 4, text: 'Juice'},
+  ]);
+
   return (
     <View style={Styles.container}>
-      <Header title='Shopping List'/>
+      <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
-  )
-}
+  );
+};
 
 const Styles = StyleSheet.create({
   container: {
-    flex:1,
-    paddingTop: 60
+    flex: 1,
+    paddingTop: 60,
   },
-})
+});
 
 export default App;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,28 +1,28 @@
-import { View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const Header = ({title}) => {
   return (
     <View style={Styles.header}>
       <Text style={Styles.text}>{title}</Text>
     </View>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-  title: 'Shopping List'
-}
+  title: 'Shopping List',
+};
 
 const Styles = StyleSheet.create({
   header: {
     height: 60,
     padding: 15,
-    backgroundColor: 'darkslateblue'
+    backgroundColor: 'darkslateblue',
   },
   text: {
     color: 'white',
     fontSize: 23,
-    textAlign: 'center'
-  }
-})
+    textAlign: 'center',
+  },
+});
 
 export default Header;
