2506a106b17f18bdf0ba0924dfee20be1d146546
Luke Climenhage <luke.climenhage@gmail.com>
2023-12-31 11:23:48 -0500

    Create AddItem component

---
+++ components/AddItem.jsx
@@ -0,0 +1,45 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+const AddItem = ({}) => {
+  return (
+    <View>
+      <TextInput placeholder="Add Item..." style={Styles.input} />
+      <TouchableOpacity style={Styles.button}>
+        <Text style={Styles.buttonText}>
+          <Icon name="plus" size={20} /> Add Item
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+Header.defaultProps = {
+  title: 'Shopping List',
+};
+
+const Styles = StyleSheet.create({
+  input: {
+    height: 60,
+    padding: 8,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: '#c2bad8',
+    padding: 9,
+    margin: 5,
+  },
+  buttonText: {
+    color: 'darkslateblue',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
+
+export default AddItem;
