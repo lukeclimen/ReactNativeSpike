93053856369051ed2346b10d5ccb51f31e7f96d8
Luke Climenhage <luke.climenhage@gmail.com>
2023-12-31 11:19:01 -0500

    Add delete functionality to ListItem

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -1,12 +1,17 @@
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
-const ListItem = ({item}) => {
+const ListItem = ({item, deleteItem}) => {
   return (
     <TouchableOpacity style={Styles.listItem}>
       <View style={Styles.listItemView}>
         <Text style={Styles.listItemText}>{item.text}</Text>
-        <Icon name="remove" size={20} color="firebrick" />
+        <Icon
+          name="remove"
+          size={20}
+          color="firebrick"
+          onPress={() => deleteItem(item.id)}
+        />
       </View>
     </TouchableOpacity>
   );
