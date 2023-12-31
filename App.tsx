8742e403d59a9aa85dbb70e6f3d86ae5b65cfdc7
Luke Climenhage <luke.climenhage@gmail.com>
2023-12-31 09:56:14 -0500

    Add StyleSheet to App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text} from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={Styles.container}>
+      <Text style={Styles.text}>Hello World</Text>
     </View>
   )
 }
+
+const Styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    color:'darkslateblue',
+    fontSize:30
+  }
+})
 
 export default App;
